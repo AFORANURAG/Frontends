b7cd345587d5c6785a94255d6af1152242220d5b
AFORANURAG <anuragupadhyay172912313@gmail.com>
2024-08-19 00:19:22 +0530

    feature: added actions package which would define our fetching and mutation logic with an extra data access and mutating layer.

---
+++ Frontend-Projects/apps/web/src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+
+export default async function Page() {
+console.log("asdasdsad")
+  return <h1>Hello, Home page! </h1>
+}
---
+++ Frontend-Projects/packages/actions/src/server/api/routers/post.ts
@@ -0,0 +1,27 @@
+import "server-only"; // Make sure you can't import this on client
+
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const postRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation( () => {
+     return {name:"Hello world."}
+      //
+    }),
+
+  delete: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation( () => {
+      return {name:"Hello world"}
+    }),
+
+
+  getPosts: publicProcedure.query(() => {
+    return {name:"Hello"}
+  }),
+
+
+});
